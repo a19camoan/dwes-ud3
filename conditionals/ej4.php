2230faa3a751bc37e4f4077c0b8dcf429e60032c
a19camoan <56250607+a19camoan@users.noreply.github.com>
2023-10-01 15:11:45 +0000

    Terminado ejercicio 5

--- conditionals/ej4.php
+++ conditionals/ej4.php
@@ -3,7 +3,7 @@
      * Modifica la página inicial realizada, incluyendo una imagen de cabecera en función de la estación del
      * año en la que nos encontremos y un color de fondo en función de la hora del día.
      * 
-     * @version 1.0.1
+     * @version 1.0.0
      * @since 27-09-2023
      * @author Andrés <a19camoan@iesgrancapitan.org>
      */
--- conditionals/ej5.php
+++ conditionals/ej5.php
@@ -4,7 +4,27 @@
      *   Perfil Administrador: Pagina1, Pagina2, Pagina3, Pagina4
      *   Perfil Usuario: Pagina1, Pagina2
      * 
-     * @version 1.0.1
+     * @version 1.0.0
      * @since 01-10-2023
      * @author Andrés <a19camoan@iesgrancapitan.org>
      */
+
+    $rol = "Usuario";
+
+    switch ($rol) {
+        case "Administrador":
+            echo "<p><a href='#'>Pagina1</a></p>";
+            echo "<p><a href='#'>Pagina2</a></p>";
+            echo "<p><a href='#'>Pagina3</a></p>";
+            echo "<p><a href='#'>Pagina4</a></p>";
+            break;
+        case "Usuario":
+            echo "<p><a href='#'>Pagina1</a></p>";
+            echo "<p><a href='#'>Pagina2</a></p>";
+            break;
+        default:
+            echo "<p>No tienes permisos para ver esta página</p>";
+            break;
+    }
+
+    echo "<a href='github.com/a19camoan/dwes-ud3' target='_blank'>Repositorio</a>";
